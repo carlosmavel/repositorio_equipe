996f6538882678dd0c8e1ecfaa9cbf68ab5d8dd7
Carlos Mavel <carlosmavel@hotmail.com>
2025-06-06 17:08:15 -0300

    fix conflicts in centro custo test

--- tests/test_centro_custo.py
+++ tests/test_centro_custo.py
@@ -12,8 +12,7 @@ def client():
     app.config['TESTING'] = True
     with app.app_context():
         db.create_all()
-        # Create establishment to satisfy FK
-        est = Estabelecimento(codigo='E1', nome_fantasia='Estab', razao_social='R', ativo=True)
+        est = Estabelecimento(codigo='EST1', nome_fantasia='Estab 1')
         db.session.add(est)
         db.session.commit()
         with app.test_client() as client:
@@ -30,16 +29,17 @@ def login_admin(client):
 
 def test_create_centro_custo(client):
     login_admin(client)
-    est = Estabelecimento.query.first()
+    with app.app_context():
+        est_id = Estabelecimento.query.first().id
     response = client.post('/admin/centros_custo', data={
-        'codigo': 'CC1',
+        'codigo': 'CC01',
         'nome': 'Centro 1',
-        'estabelecimento_id': est.id,
+        'estabelecimento_id': est_id,
         'ativo_check': 'on'
     }, follow_redirects=True)
     assert response.status_code == 200
     with app.app_context():
-        cc = CentroDeCusto.query.filter_by(codigo='CC1').first()
+        cc = CentroDeCusto.query.filter_by(codigo='CC01').first()
         assert cc is not None
+        assert cc.estabelecimento_id == est_id
         assert cc.nome == 'Centro 1'
-        assert cc.ativo is True
