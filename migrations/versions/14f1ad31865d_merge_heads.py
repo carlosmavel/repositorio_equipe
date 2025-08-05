225aa0d6ae2a74b291b83f8bea3c00b5de5b471b
Carlos Mavel <carlosmavel@hotmail.com>
2025-08-05 12:03:38 -0300

    chore(migrations): adiciona arquivo de merge para resolver múltiplas heads

---
+++ migrations/versions/14f1ad31865d_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: 14f1ad31865d
+Revises: 158a16b634c0, 1b2c3d4e5f67
+Create Date: 2025-08-05 11:58:15.914800
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '14f1ad31865d'
+down_revision = ('158a16b634c0', '1b2c3d4e5f67')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
